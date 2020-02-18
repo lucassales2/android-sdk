f4abc6f1e611ac19c75b0b9714c1d0a2c8a99cc4
lucassales <saless182@gmail.com>
2020-02-18 14:41:18 +0100

    Codegen

---
+++ nstack-kotlin-core/src/main/java/dk/nodes/nstack/kotlin/codegen/tools/CollectionFormats.kt
@@ -0,0 +1,20 @@
+package dk.nodes.nstack.kotlin.codegen.tools
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CSV
+
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class SSV
+
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class TSV
+
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PIPES
